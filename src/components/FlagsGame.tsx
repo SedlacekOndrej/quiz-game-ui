7cb9843f78b5c6501a5d4b71f7bdb88db5109a8c
c2302004 <ondrej_sedlacek@wistron.com>
2023-07-04 14:57:25 +0200

    trying to make the code shorter in flags game

--- src/components/FlagsGame.tsx
+++ src/components/FlagsGame.tsx
@@ -1,5 +1,5 @@
 import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
-import { ChangeEvent } from "react";
+import { ChangeEvent, Fragment } from "react";
 
 interface FlagsGameProps {
     flags: string[]
@@ -103,6 +103,19 @@ export default function FlagsGame(props: FlagsGameProps) {
                 <FormControlLabel value={states[38]} control={<Radio />} label={states[38]} disabled={timeOut} />
                 <FormControlLabel value={states[39]} control={<Radio />} label={states[39]} disabled={timeOut} />
             </RadioGroup>
+            {flags.map(flag =>
+                <Fragment>
+                    <FormLabel sx={{ mt: 5, fontWeight: "bold", fontSize: 20 }} id="question">
+                        <img style={{ width: "100px", height: "100px" }} src={`https://flagsapi.com/${flags[flags.indexOf(flag)]}/shiny/64.png`} alt="flag10"></img>
+                    </FormLabel>
+                    <RadioGroup sx={{ mt: 1 }} aria-labelledby="question" name={"answer".concat((flags.indexOf(flag) + 1).toString())} onChange={handleChange} row>
+                        <FormControlLabel value={states[flags.indexOf(flag) * 4]} control={<Radio />} label={states[flags.indexOf(flag) * 4]} disabled={timeOut} />
+                        <FormControlLabel value={states[(flags.indexOf(flag) * 4) + 1]} control={<Radio />} label={states[(flags.indexOf(flag) * 4) + 1]} disabled={timeOut} />
+                        <FormControlLabel value={states[(flags.indexOf(flag) * 4) + 2]} control={<Radio />} label={states[(flags.indexOf(flag) * 4) + 2]} disabled={timeOut} />
+                        <FormControlLabel value={states[(flags.indexOf(flag) * 4) + 3]} control={<Radio />} label={states[(flags.indexOf(flag) * 4) + 3]} disabled={timeOut} />
+                    </RadioGroup>
+                </Fragment>
+            )}
         </FormControl>
     );
 }
